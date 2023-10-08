705b198ccebd4fd5246e49c87f7f7f984a9e8b02
Chems-Eddibe Rebouh <drebouh123@gmail.com>
2023-10-07 20:49:21 -0400

    Add project files.

---
+++ Tp1LucasChems/Tp1LucasChems.cpp
@@ -0,0 +1,13 @@
+// Tp1LucasChems.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    const int NB_ELEMENTS = 10;
+    int tableau1[] = { 22, 44, 12, 61, 4, 99, 33, 17, 15, 123 };
+    int tableau2[] = { 22, 33, 44, 61, 4, 99, 12, 17, 123, 15 };
+    int tableau3[] = { 22, 44, 12, 61, 4, 99, 33, 17, 15, 123 };
+
+}
